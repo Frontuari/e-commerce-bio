9bb9d021a0352f0e991ce473c06986b29f9fc53b
Carlos Vargas <cvargas@frontuari.net>
2020-01-06 23:50:28 -0400

    organizando la plantilla y separando cada seccion

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,243 @@
+<footer class="section footer">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-3">
+					<div class="footer-block">
+						<img src="assets/img/logo-bio-mercados.svg" alt="Bio Mercados">
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+						<p><span>Teléfono:</span> +58 414 123 4567</p>
+						<p><span>Correo:</span> Contacto@Biomercados.com</p>
+					</div>
+				</div>
+				<div class="col-lg-2">
+					<div class="footer-block">
+						<a class="btn btn-title" data-toggle="collapse" href="#enlaces" role="button" aria-expanded="false" aria-controls="enlaces">
+							Enlaces Importantes
+						</a>
+						<div class="collapse" id="enlaces">
+							<div class="card card-body">
+								<a href="#">Preguntas Frecuentes</a>
+								<a href="#">Términos legales</a>
+								<a href="#">Soporte</a>
+								<a href="#">Contacto</a>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-3">
+					<div class="footer-block">
+						<a class="btn btn-title" data-toggle="collapse" href="#apps" role="button" aria-expanded="false" aria-controls="apps">
+							Descarga nuestras <br class="on-desktop">app's móviles
+						</a>
+						<div class="collapse" id="apps">
+							<div class="card card-body">
+								<a href="#"><img src="assets/img/descarga-app-android-bio.svg"></a>
+								<a href="#"><img src="assets/img/descarga-app-ios-bio.svg"></a>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-4">
+					<div class="footer-block">
+						<a class="btn btn-title" data-toggle="collapse" href="#suscripcion" role="button" aria-expanded="false" aria-controls="suscripcion">
+						   Suscríbete para recibir notificaciones
+						</a>
+						<div class="collapse" id="suscripcion">
+							<div class="card card-body">
+								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus blanditiis ipsa et odio.</p>
+								<div class="footer-form">
+									<form action="">
+										<div class="form-group">
+											<div class="input-group">
+												<input type="text" class="form-control" placeholder="Escribe tu correo aquí..." aria-label="Escribe tu correo aquí...">
+												<div class="input-group-append">
+													<button class="btn" type="button" id="button-addon2">Suscribirse</button>
+												</div>
+											</div>
+										</div>
+									</form>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="bottom-footer">
+			<div class="container">
+				<p>Todos los derechos reservados. Bio Mercados 2020</p>
+			</div>
+		</div>
+	</footer>
+
+	<div class="modal fade" id="ModalProd" tabindex="-1" role="dialog" aria-labelledby="ModalProdLabel" aria-hidden="true">
+		<div class="modal-dialog" role="document">
+			<div class="modal-content">
+				<div class="modal-body">
+					<div class="product-block">
+						<div class="row">
+							<div class="col-lg-5">
+								<div class="product-img">
+									<img src="assets/img/producto-bio-002.jpg">
+								</div>
+							</div>
+							<div class="col-lg-7">
+								<div class="product-description">
+									<a href="#" class="product-title">Carne de Res</a>
+									<span class="product-info">1 Kg</span>
+									<div class="product-prices">
+										<span class="product-descount">$ 3 / Bs 135.000</span>
+										<p>$ 2 / Bs 90.000</p>
+									</div>
+								</div>
+								<div class="product-options">
+									<form action="">
+										<div class="product-quantity">
+											<label>Cantidad</label>
+											<div class="product-quantity-group">
+												<input id="quantity" class="form-control" type="text" name="quantity" value="1">
+												<div class="product-quantity-buttons">
+													<button type="button" class="btn" onclick="increaseValue()">
+														<img src="assets/img/increase.png" alt="Increase">
+													</button>
+													<button type="button" class="btn" onclick="decreaseValue()">
+														<img src="assets/img/decrease.png" alt="decrease">
+													</button>
+												</div>
+											</div>
+										</div>
+										<div class="product-prices">
+											<p><b>Total:</b> $ 2 / Bs 90.000</p>
+										</div>
+										<div class="product-buttons">
+											<button type="submit" class="btn btn-addcart">Añadir al carrito</button>
+											<button type="button" class="btn btn-addfavorite">
+												<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 14.93 15"><defs><style>.cls-1{fill:#ccc;}</style></defs><title>añadir-favorito-bio</title><g id="Capa_2" data-name="Capa 2"><g id="Guias_y_recursos" data-name="Guias y recursos"><path class="cls-1" d="M4.7,7.56a.42.42,0,0,1-.42-.42V3.5H14.51a.43.43,0,0,1,0,.85H5.13V7.14A.42.42,0,0,1,4.7,7.56Z"/><path class="cls-1" d="M14.93,15H7.19a.43.43,0,0,1,0-.85h6.9V5.09a.42.42,0,1,1,.84,0Z"/><path class="cls-1" d="M11.53,6a.42.42,0,0,1-.42-.43V2a1,1,0,0,0-.43-.84A1.86,1.86,0,0,0,9.6.85C9,.85,8,1.15,8,2V5.53a.42.42,0,1,1-.84,0V2A2.18,2.18,0,0,1,9.6,0,2.12,2.12,0,0,1,12,2V5.53A.43.43,0,0,1,11.53,6Z"/><path class="cls-1" d="M8.74,8.11a2.23,2.23,0,0,0-1.63-.77A3.6,3.6,0,0,0,4.7,8.39,3.58,3.58,0,0,0,2.3,7.34a2.23,2.23,0,0,0-1.63.77A2.51,2.51,0,0,0,0,10.31C.32,12,2,13.69,4.52,15a.39.39,0,0,0,.18,0,.41.41,0,0,0,.19,0c2.57-1.27,4.2-3,4.48-4.65A2.51,2.51,0,0,0,8.74,8.11Zm-.21,2.06c-.1.66-.7,2.34-3.83,3.93C1.57,12.51,1,10.83.87,10.17a1.68,1.68,0,0,1,.4-1.47h0a1.39,1.39,0,0,1,1-.51h0a3.14,3.14,0,0,1,2,1.09.44.44,0,0,0,.6,0A3.15,3.15,0,0,1,7.09,8.18a1.41,1.41,0,0,1,1,.51h0A1.63,1.63,0,0,1,8.53,10.17Z"/></g></g></svg>
+											</button>
+										</div>
+									</form>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="modal fade" id="ModalProdCombo" tabindex="-1" role="dialog" aria-labelledby="ModalProdComboLabel" aria-hidden="true">
+		<div class="modal-dialog" role="document">
+			<div class="modal-content">
+				<div class="modal-body">
+					<div class="product-block">
+						<div class="row">
+							<div class="col-lg-5">
+								<div class="product-img">
+									<img src="assets/img/combo-bio-001.jpg">
+								</div>
+							</div>
+							<div class="col-lg-7">
+								<div class="product-description">
+									<a href="#" class="product-title">Cesta bio</a>
+									<span class="product-info">2 Harinas de maíz blanco - 1 kg</span>
+									<span class="product-info">1 Arroz blanco tipo 1 - 1 kg</span>
+									<span class="product-info">1 Pasta larga o corto - 1 kg</span>
+									<span class="product-info">1 Margarina - 500 g</span>
+									<span class="product-info">1 Aceite de maíz o soya - 1 L</span>
+									<span class="product-info">1 Harina de trigo - 1 kg</span>
+									<span class="product-info">1 Azúcar refinada - 1 kg</span>
+									<span class="product-info">1 Sal de mar - 1 kg</span>
+									<span class="product-info">1 Café - 500 g</span>
+									<span class="product-info">1 Vinagre - 500 ml</span>
+									<span class="product-info">1 Fororo - 900 g</span>
+									<span class="product-info">1 Leche en polvo - 400 g</span>
+									<span class="product-info">2 Sardinas de lata - 260 g</span>
+									<span class="product-info">1 Granos - 500 g</span>
+									<div class="product-prices">
+										<span class="product-descount">$ 3 / Bs 135.000</span>
+										<p>$ 2 / Bs 90.000</p>
+									</div>
+								</div>
+								<div class="product-options">
+									<form action="">
+										<div class="product-quantity">
+											<label>Cantidad</label>
+											<div class="product-quantity-group">
+												<input id="quantity2" class="form-control" type="text" name="quantity" value="1">
+												<div class="product-quantity-buttons">
+													<button type="button" class="btn" onclick="increaseValue2()">
+														<img src="assets/img/increase.png" alt="Increase">
+													</button>
+													<button type="button" class="btn" onclick="decreaseValue2()">
+														<img src="assets/img/decrease.png" alt="decrease">
+													</button>
+												</div>
+											</div>										
+										</div>
+										<div class="product-prices">
+											<p><b>Total:</b> $ 2 / Bs 90.000</p>
+										</div>
+										<div class="product-buttons">
+											<button type="submit" class="btn btn-addcart">Añadir al carrito</button>
+											<button type="button" class="btn btn-addfavorite">
+												<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 14.93 15"><defs><style>.cls-1{fill:#ccc;}</style></defs><title>añadir-favorito-bio</title><g id="Capa_2" data-name="Capa 2"><g id="Guias_y_recursos" data-name="Guias y recursos"><path class="cls-1" d="M4.7,7.56a.42.42,0,0,1-.42-.42V3.5H14.51a.43.43,0,0,1,0,.85H5.13V7.14A.42.42,0,0,1,4.7,7.56Z"/><path class="cls-1" d="M14.93,15H7.19a.43.43,0,0,1,0-.85h6.9V5.09a.42.42,0,1,1,.84,0Z"/><path class="cls-1" d="M11.53,6a.42.42,0,0,1-.42-.43V2a1,1,0,0,0-.43-.84A1.86,1.86,0,0,0,9.6.85C9,.85,8,1.15,8,2V5.53a.42.42,0,1,1-.84,0V2A2.18,2.18,0,0,1,9.6,0,2.12,2.12,0,0,1,12,2V5.53A.43.43,0,0,1,11.53,6Z"/><path class="cls-1" d="M8.74,8.11a2.23,2.23,0,0,0-1.63-.77A3.6,3.6,0,0,0,4.7,8.39,3.58,3.58,0,0,0,2.3,7.34a2.23,2.23,0,0,0-1.63.77A2.51,2.51,0,0,0,0,10.31C.32,12,2,13.69,4.52,15a.39.39,0,0,0,.18,0,.41.41,0,0,0,.19,0c2.57-1.27,4.2-3,4.48-4.65A2.51,2.51,0,0,0,8.74,8.11Zm-.21,2.06c-.1.66-.7,2.34-3.83,3.93C1.57,12.51,1,10.83.87,10.17a1.68,1.68,0,0,1,.4-1.47h0a1.39,1.39,0,0,1,1-.51h0a3.14,3.14,0,0,1,2,1.09.44.44,0,0,0,.6,0A3.15,3.15,0,0,1,7.09,8.18a1.41,1.41,0,0,1,1,.51h0A1.63,1.63,0,0,1,8.53,10.17Z"/></g></g></svg>
+											</button>
+										</div>
+									</form>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="over toggle-menu"></div>
+	<script type="text/javascript" src="{{ asset('assets/js/jquery-3.3.1.min.js') }}"></script>
+	<script type="text/javascript" src="{{ asset('assets/js/popper.js') }}"></script>
+	<script type="text/javascript" src="{{ asset('assets/js/bootstrap.min.js') }}"></script>
+	@yield('js')
+	<script>
+		jQuery(document).ready(function($){
+			$(".toggle-menu").click(function(){
+				$(".over").toggle();
+				$(".navbar").toggleClass("navbar-translate");
+			});
+		});
+
+		function increaseValue() {
+		  var value = parseInt(document.getElementById('quantity').value, 10);
+		  value = isNaN(value) ? 0 : value;
+		  value++;
+		  document.getElementById('quantity').value = value;
+		}
+
+		function decreaseValue() {
+		  var value = parseInt(document.getElementById('quantity').value, 10);
+		  value = isNaN(value) ? 0 : value;
+		  value < 1 ? value = 1 : '';
+		  value--;
+		  document.getElementById('quantity').value = value;
+		}
+
+		/*DUPLICADA SOLO PARA EL DEMO, ELIMINAR DURANTE LA PROGRAMACIÓN*/
+
+
+		function increaseValue2() {
+		  var value = parseInt(document.getElementById('quantity2').value, 10);
+		  value = isNaN(value) ? 0 : value;
+		  value++;
+		  document.getElementById('quantity2').value = value;
+		}
+
+		function decreaseValue2() {
+		  var value = parseInt(document.getElementById('quantity2').value, 10);
+		  value = isNaN(value) ? 0 : value;
+		  value < 1 ? value = 1 : '';
+		  value--;
+		  document.getElementById('quantity2').value = value;
+		}
+	</script>
+</body>
+</html>
