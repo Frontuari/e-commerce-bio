dc1d89f957a25b8785e42f2c6f502728a87612c9
webmasterscity <ds000082@gmail.com>
2020-01-27 22:09:47 -0400

    formateos_numeros_and_fix_migration

--- app/Orders.php
+++ app/Orders.php
@@ -3,8 +3,64 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-
+use App\FuncionesGenerales;
 class Orders extends Model
 {
-    //
+
+    public function getTotaltaxAttribute($value)
+    {
+        $fg= new FuncionesGenerales;
+        return $fg->get_formato_moneda($value);
+    }
+    public function setTotaltaxAttribute($value)
+    {
+        $fg= new FuncionesGenerales;
+        $this->attributes['total_tax']=$fg->set_formato_moneda($value);
+    }
+    public function getSubTotalAttribute($value){
+        $fg= new FuncionesGenerales;
+        return $fg->get_formato_moneda($value);
+     }
+     public function setSubTotalAttribute($value)
+     {
+         $fg= new FuncionesGenerales;
+         $this->attributes['sub_total']=$fg->set_formato_moneda($value);
+     }
+     public function getTotalPackagingAttribute($value){
+        $fg= new FuncionesGenerales;
+        return $fg->get_formato_moneda($value);
+     }
+     public function setTotalPackagingAttribute($value)
+     {
+         $fg= new FuncionesGenerales;
+         $this->attributes['total_packaging']=$fg->set_formato_moneda($value);
+     }
+     public function setTotalTransportAttribute($value)
+     {
+         $fg= new FuncionesGenerales;
+         $this->attributes['total_transport']=$fg->set_formato_moneda($value);
+     }
+     public function getTotalTransportAttribute($value){
+        $fg= new FuncionesGenerales;
+        return $fg->get_formato_moneda($value);
+     }
+
+     public function setTotalPayAttribute($value)
+     {
+         $fg= new FuncionesGenerales;
+         $this->attributes['total_pay']=$fg->set_formato_moneda($value);
+     }
+     public function getTotalPayAttribute($value){
+        $fg= new FuncionesGenerales;
+        return $fg->get_formato_moneda($value);
+     }
+     public function setCurrencyRateAttribute($value)
+     {
+         $fg= new FuncionesGenerales;
+         $this->attributes['currency_rate']=$fg->set_formato_moneda($value);
+     }
+     public function getCurrencyRateAttribute($value){
+        $fg= new FuncionesGenerales;
+        return $fg->get_formato_moneda($value);
+     }
 }
