f679c98ccd197c3dfe02c0971ab1239ed3b580c6
webmasterscity <ds000082@gmail.com>
2020-02-10 15:27:56 -0400

    secuencias

--- public/reparar_secuencias.php
+++ public/reparar_secuencias.php
@@ -1,14 +1,56 @@
 <?php
 
-require __DIR__.'/../vendor/autoload.php';
+require '../vendor/autoload.php';
 
+/*
 $base_dato=getenv("DB_DATABASE");
 $usuario=getenv("DB_USERNAME");
 $clave=getenv("DB_PASSWORD");
-$puerto=getenv("DB_PORT");
+$puerto=getenv("DB_PORT"):
+*/
+$base_dato=$_GET['db'];
+$usuario=$_GET['user'];
+$clave=$_GET['clave'];
+$puerto="5432";
+
 $dbconn = pg_connect("host=localhost dbname=$base_dato user=$usuario password=$clave port=$puerto")
 or die('No se ha podido conectar: ' . pg_last_error());
+$query="SELECT * FROM pg_catalog.pg_tables";
+$result = pg_query($query) or die('La consulta fallo: ' . pg_last_error());
+while ($line = pg_fetch_array($result, null, PGSQL_ASSOC)) {
+  
+        
+       $sc=$line['schemaname'];
+       if($sc=='public'){
 
+        echo $line['tablename']."<br>";
+        $tabla= $line['tablename'];
+        $query="SELECT MAX(id) max FROM ".$tabla;
+        $res=pg_query($query);
+        $lineb = pg_fetch_array($res, null, PGSQL_ASSOC);
+        $max=$lineb['max'];
+        if($max==0)
+        echo "0<br>";
+        else{
+            $max++;
+            $query="alter SEQUENCE ".$tabla."_id_seq RESTART with $max";
+            echo pg_query($query);
+        }
+        
+       }
+      
+    
+    
+    
+    }
+exit();
+
+
+
+
+$result = pg_query($query) or die('La consulta fallo: ' . pg_last_error());
+print_r($result);
+exit();
 // Realizando una consulta SQL
 $query = "
 SELECT 'SELECT SETVAL(' ||
