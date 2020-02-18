2226d6ddf8243a477c2f740361dff4aeb8757383
webmasterscity <ds000082@gmail.com>
2020-02-17 20:50:17 -0400

    api_rapida

---
+++ public/api_rapida.php
@@ -0,0 +1,103 @@
+<?php
+cabecera('On');
+
+$a=extraer_datos_db();
+conectar_db($a['host'],$a['database'],$a['user'],$a['password'],$a['port']);
+$datos=run();
+session_start();
+$_GET=seguro($_GET);
+$_POST=seguro($_POST);
+switch($_GET['evento']){
+
+    case 'login':
+        $email=$_GET['email'];
+        $clave=$_GET['clave'];
+        
+        $row=q("SELECT id,password,email FROM users WHERE email='$email'")[0];
+       if($row['email']){
+            if(password_verify($clave,$row['password'])){
+                $_SESSION['user_id']=$row['id'];
+                $row['id_sesion']=session_id();
+                echo json_encode($row);
+            }else{
+                //clave no valida
+                echo "clave no valida";
+                return false;
+            }
+       }else{
+           //email no valido
+           echo "email no valido";
+       return false;
+    }
+    break;
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function extraer_datos_db(){
+    $gestor = @fopen("../.env", "r");
+    if ($gestor) {
+        while (($búfer = fgets($gestor, 4096)) !== false) {
+            $arr[]=$búfer;
+        }
+        fclose($gestor);
+    }
+    $db['host']     =limpiar($arr[9]);
+    $db['port']     =limpiar($arr[10]);
+    $db['database'] =limpiar($arr[11]);
+    $db['user']     =limpiar($arr[12]);
+    $db['password'] =limpiar($arr[13]);
+    return $db;
+}
+function limpiar($var){
+    return trim(explode('=',$var)[1]);
+}
+function conectar_db($host,$base_dato,$usuario,$clave,$puerto){
+    $dbconn = pg_connect("host=".$host." dbname=$base_dato user=$usuario password=$clave port=$puerto")
+    or die('No se ha podido conectar: ' . pg_last_error());
+}
+function q($sql){
+    $result = pg_query($sql) or die(false);
+    while ($line = pg_fetch_array($result, null, PGSQL_ASSOC)) {
+        $arr[]=$line;
+    }
+    return $arr;
+}
+function cabecera($error="Off"){
+    header('Access-Control-Allow-Origin: *');
+    header("Access-Control-Allow-Credentials: true");
+    header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
+    header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
+    header("Access-Control-Allow-Headers: content-type, authorization");
+    ini_set('display_errors',$error);
+    error_reporting(E_ALL ^E_NOTICE ^E_DEPRECATED);
+}
+function run(){
+    $postdata = file_get_contents("php://input");
+    $input = json_decode($postdata);
+    if($_GET['id_sesion']){
+        session_id($_GET['id_sesion']);
+    }
+    return $input;
+}
+function seguro($varb){
+    foreach($varb as $id=>$var){
+        $_GET[$id]= pg_escape_string(htmlspecialchars(filter_var($var,FILTER_SANITIZE_STRING)));
+    }
+    return $_GET;
+}
+
+?>
