c2012f393a012cc90b629025dafa29d14f8c3890
Leonardo Alvarado <softvenca@gmail.com>
2020-02-21 19:39:47 -0400

    envio de correo desde el registro

---
+++ resources/views/email/verification.blade.php
@@ -0,0 +1,12 @@
+@component('mail::message')
+
+<h1>¡Bienvenido a Biomercados!</h1>
+<p>Para verificar tu email, por favor haz click en el botón, sigue este enlace</p>
+
+<sub>Gracias</sub>
+<br>
+<sub>El equipo de Biomercados</sub>
+
+
+
+@endcomponent
