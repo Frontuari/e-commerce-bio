1f4d2b3cc0b32e56af04ec1c38308c04c2769de9
Softvenca <softvenca@gmail.com>
2020-04-08 13:03:27 -0400

    header json contenttype

--- public/getProducts.php
+++ public/getProducts.php
@@ -1,5 +1,5 @@
 <?php
-	// header("Content-type: application/json; charset=utf-8");
+	header("Content-type: application/json; charset=utf-8");
 	ini_set('max_execution_time', '600');
 	ini_set("display_errors", "on");
 	error_reporting(-1);
