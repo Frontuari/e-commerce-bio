cb2bb3f9e346041e10479f580f547743b0c915ee
webmasterscity <ds000082@gmail.com>
2020-04-20 18:50:36 -0400

    formulario_peso_transporte

--- database/seeds/TransportsTableSeeder.php
+++ database/seeds/TransportsTableSeeder.php
@@ -19,17 +19,6 @@ class TransportsTableSeeder extends Seeder
         \DB::table('transports')->insert(array (
             0 => 
             array (
-                'id' => 2,
-                'name' => 'Así de Rápido',
-                'description' => 'Despacho inmediato.',
-                'price' => '150000.000000',
-                'time_minutes' => 0,
-                'status' => 'A',
-                'created_at' => '2020-01-24 16:11:50',
-                'updated_at' => '2020-04-06 16:23:45',
-            ),
-            1 => 
-            array (
                 'id' => 1,
                 'name' => 'Retirar en tienda',
                 'description' => 'Debe esperar por lo menos 30 minutos antes de dirigirse a nuestra tienda Bio y retirar su pedido.',
@@ -38,6 +27,19 @@ class TransportsTableSeeder extends Seeder
                 'status' => 'A',
                 'created_at' => NULL,
                 'updated_at' => '2020-04-15 18:36:18',
+                'peso_max' => '15',
+            ),
+            1 => 
+            array (
+                'id' => 2,
+                'name' => 'Así de Rápido',
+                'description' => 'Despacho inmediato.',
+                'price' => '150000.000000',
+                'time_minutes' => 0,
+                'status' => 'A',
+                'created_at' => '2020-01-24 16:11:50',
+                'updated_at' => '2020-04-20 22:38:40',
+                'peso_max' => '15',
             ),
         ));
         
