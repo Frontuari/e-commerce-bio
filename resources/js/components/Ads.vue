715e319d21f516a0800ae04196e33d20ce2973a9
Carlos Vargas <cvargas@frontuari.net>
2020-04-30 16:49:31 -0400

    add the style to category images

--- resources/js/components/Ads.vue
+++ resources/js/components/Ads.vue
@@ -3,16 +3,16 @@
 		<div class="container-fluid">
 			<div class="row">
 				<div class="col-md-3">
-					<a :href="'catalog?cat='+categories[0].id"><div class="category-img" :style="{backgroundImage: backgrounds[0]}"></div></a>
+					<a :href="'catalog?cat='+categories[0].id"><div class="category-img" :style="{backgroundImage: backgrounds[0],backgroundSize: 'contain'}"></div></a>
 				</div>
 				<div class="col-md-3">
-					<a :href="'catalog?cat='+categories[1].id"><div class="category-img" :style="{backgroundImage: backgrounds[1]}"></div></a>
+					<a :href="'catalog?cat='+categories[1].id"><div class="category-img" :style="{backgroundImage: backgrounds[1], backgroundSize: 'contain'}"></div></a>
 				</div>
 				<div class="col-md-3">
-					<a :href="'catalog?cat='+categories[2].id"><div class="category-img" :style="{backgroundImage: backgrounds[2]}"></div></a>
+					<a :href="'catalog?cat='+categories[2].id"><div class="category-img" :style="{backgroundImage: backgrounds[2], backgroundSize: 'contain'}"></div></a>
 				</div>
 				<div class="col-md-3">
-					<a :href="'catalog?cat='+categories[3].id"><div class="category-img" :style="{backgroundImage: backgrounds[3]}"></div></a>
+					<a :href="'catalog?cat='+categories[3].id"><div class="category-img" :style="{backgroundImage: backgrounds[3], backgroundSize: 'contain'}"></div></a>
 				</div>
 			</div>
 		</div>
