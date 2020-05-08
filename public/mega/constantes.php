1e949ee89bc362f86d2ddb69c7634275fe7b14ed
webmasterscity <ds000082@gmail.com>
2020-05-08 10:00:18 -0400

    megasoft prueba

--- public/mega/constantes.php
+++ public/mega/constantes.php
@@ -1,6 +1,10 @@
 <?php
-    define('USERNAME', 'sanitas01');
-    define('PASSWORD', 'Mega123-+');
-    define('CODAFILIACION', '2013061801');
+    //define('USERNAME', 'sanitas01');
+    //define('PASSWORD', 'Mega123-+');
+    //define('CODAFILIACION', '2013061801');
+    define('USERNAME', 'biomer01');
+    define('PASSWORD', 'Biopg02*');
+    define('CODAFILIACION', '25032020');
     define('URL_MEGA','https://paytest.megasoft.com.ve:8443');
 ?>
+
