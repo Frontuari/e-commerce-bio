dace0d6c8899b7bb84beeec121f8d5690c60bf59
webmasterscity <ds000082@gmail.com>
2020-05-08 14:53:37 -0400

    megasoft 6

--- public/mega/pago_procesado.php
+++ public/mega/pago_procesado.php
@@ -2,12 +2,12 @@
     include_once './Clases/Login.php';
     include_once './Clases/LeerXML.php';
     include './constantes.php';
-    
+   
     $archivo=fopen("log.txt",'a+');
     fwrite($archivo,"PaginaWeb: Llegando a nuestro servidor\n");
     
     $numeroControl=$_GET['control'];
-    
+    echo $numeroControl;
     fwrite($archivo,"Control: $numeroControl\n");
     
     $url=URL_MEGA."/payment/action/paymentgatewayuniveral-querystatus?control=$numeroControl";
@@ -15,17 +15,17 @@
     fwrite($archivo,"URL de petición de XML: $url\n");
     
     $control = new Login();
-    $leerXML = new LeerXML();
     
+
     fwrite($archivo,"Obteniendo Login\n");
     fwrite($archivo,"Datos a enviar:\n");
     fwrite($archivo,"URL:".$url." USERNAME: ".USERNAME." PASSWORD: ".PASSWORD."\n");
     
     $xml=$control->loginHTTPS($url,USERNAME,PASSWORD);
-    
+   
     fwrite($archivo,"Resultado $xml \n");
     
     fclose($archivo);
-    $leerXML->leerXML($xml);
+    $leerXML= new leerXML($xml);
     
 ?>
