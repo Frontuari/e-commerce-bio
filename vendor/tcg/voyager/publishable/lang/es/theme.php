9fcad0eb7263b3d5cd2d8988e586b3f471cf1bd5
webmasterscity <ds000082@gmail.com>
2020-06-11 11:30:19 -0400

    fix

--- vendor/tcg/voyager/publishable/lang/es/theme.php
+++ vendor/tcg/voyager/publishable/lang/es/theme.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-    'footer_copyright'  => 'Hecho con <i class = "voyager-heart"> </i> por',
-    'footer_copyright2' => 'Hecho con ron e incluso más ron',
+    'footer_copyright'  => 'Hora del servidor: '.Date('d-m-Y H:i:s A').', Hecho con <i class = "voyager-heart"> </i> por',
+    'footer_copyright2' => 'Hecho con ron e incluso más ron que ese sto???',
 ];
