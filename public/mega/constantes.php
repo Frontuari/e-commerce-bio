733898edbfbd55a325fb9fc52482bece2514beb1
webmasterscity <ds000082@gmail.com>
2020-06-16 16:32:57 -0400

    produccion megasoft

--- public/mega/constantes.php
+++ public/mega/constantes.php
@@ -4,7 +4,9 @@
     //define('CODAFILIACION', '2013061801');
     define('USERNAME', 'biomer01');
     define('PASSWORD', 'Biopg02*');
-    define('CODAFILIACION', '25032020');
-    define('URL_MEGA','https://paytest.megasoft.com.ve:8443');
+    //define('CODAFILIACION', '25032020');
+    define('CODAFILIACION', '317219686');
+    //define('URL_MEGA','https://paytest.megasoft.com.ve:8443');
+    define('URL_MEGA','https://e-payment.megasoft.com.ve');
 ?>
 
