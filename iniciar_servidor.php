4769f77d683322fdde2a65f5882191c6715c564d
Carlos Vargas <cvargas@frontuari.net>
2020-08-03 15:46:34 -0400

    add more timeout in curl setup in post method

--- iniciar_servidor.php
+++ iniciar_servidor.php
@@ -642,7 +642,7 @@ $ch = curl_init();
 
 curl_setopt($ch, CURLOPT_URL,$url);
 curl_setopt($ch, CURLOPT_POST, 1);
-curl_setopt($ch, CURLOPT_TIMEOUT, 60);
+curl_setopt($ch, CURLOPT_TIMEOUT, 360);
 curl_setopt($ch, CURLOPT_POSTFIELDS, 
 http_build_query($arr));
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
