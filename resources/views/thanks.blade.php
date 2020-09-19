b00d2c63d7e9e3b5c198755b5eb3cbb43d4e4531
= <=>
2020-09-19 10:56:33 -0400

    add the save payment

--- resources/views/thanks.blade.php
+++ resources/views/thanks.blade.php
@@ -35,18 +35,22 @@
 <script type="text/javascript">
 
 	$(document).ready(function(){
+		var orderno = localStorage.getItem('orderno');
 		$("#btn_thanks").click(function(){
 			
 		});
 
-		var orderno = localStorage.getItem('orderno');
-		var urlServicio = "{{ url('/123pago/getdataresponse') }}/";
-		urlServicio = urlServicio.replace('http:','https:');
-		
-		$.get(urlServicio+orderno, function(data){
-            let d = data;
-            Print.postMessage(d);
-        });
+		if(orderno){
+			$("#btn_thanks").hide();
+			var urlServicio = "{{ url('/123pago/getdataresponse') }}/";
+			urlServicio = urlServicio.replace('http:','https:');
+			$.get(urlServicio+orderno, function(data){
+	            let d = data;
+	            Print.postMessage(d);
+	        });
+	        localStorage.clear();
+		}
+
 	});
 </script>
 </body>
