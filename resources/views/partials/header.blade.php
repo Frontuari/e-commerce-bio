59d0a4d4bcc3d22e285f8d938419ac484d855b24
= <=>
2020-09-21 16:37:52 -0400

    add the mimify the css files

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -6,9 +6,10 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="Shortcut Icon" href="/assets/img/favicon.png" type="image/x-icon" />	
 	<link rel="stylesheet" type="text/css" href="{{ mix('css/app.css') }}">
-	<link rel="stylesheet" type="text/css" href="{{ asset('assets/css/style.css') }}?ver=1.0.8">
+	<!--<link rel="stylesheet" type="text/css" href="{{ asset('assets/css/style.min.css') }}?ver=1.0.8">-->
+	<link rel="stylesheet" type="text/css" href="{{ asset('assets/css/style.min.css') }}?ver=1.0.8">
 	<link rel="stylesheet" type="text/css" href="{{ asset('assets/css/bree-font.css') }}">
-	<link rel="stylesheet" type="text/css" href="{{ asset('css/new-styles.css') }}?ver=2.0.4">
+	<!--<link rel="stylesheet" type="text/css" href="{{ asset('css/new-styles.css') }}?ver=2.0.4">-->
 
 	<!-- Global site tag (gtag.js) - Google Analytics -->
 	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-171491944-1"></script>
