37e173c8d095ef4055cd76ac423da2efc9cab482
= <=>
2020-09-21 17:44:38 -0400

    add the hide hours in delivery

--- public/js/carrito.js
+++ public/js/carrito.js
@@ -845,7 +845,7 @@ function deli_type(e){
 		document.getElementById('direccion_selected').selectedIndex = 0;
 	}
 
-	if(parseInt(e.value) == 2){
+	if(parseInt(e.value) == 2 || parseInt(e.value) == 1){
 		document.getElementById("div_contenedor_fecha").style.display = 'none';
 	}else{
 		document.getElementById("div_contenedor_fecha").style.display = 'block';
