70f5dba678ca9642612f32f0c44c37294fa9d34c
= <=>
2020-10-19 11:22:58 -0400

    delete log 123pago

--- public/123pago_retorno.php
+++ public/123pago_retorno.php
@@ -9,11 +9,9 @@
 	$total_amount = number_format(($data['amount']*$coins[0]['rate']),2,'.','');
 	$coins_id = $coins[0]['id'];
 
-	file_put_contents("prueba_desde_el_movil.txt", json_encode($data));
-
-	//if(isset($data['parametro4']) and !empty($data['parametro4'])){
+	if(isset($data['parametro4']) and !empty($data['parametro4'])){
 		guardarPago($total_amount,$data['nai'], 15, $data['parametro4'], $user[0]['users_id'], $coins_id);
-	//}
+	}
 
 	/*-------------------------functions for operations-------------------------------*/
 
