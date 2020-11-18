16a5b8188bbf068e1f92c754bc91455e849ab2ef
Carlos Vargas <cvargas@frontuari.net>
2020-11-18 14:15:25 -0400

    change version carrito.js

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -104,7 +104,7 @@
 	<script id="aaa" type="text/javascript" src="{{ mix('js/app.js') }}"></script>
 	<script type="text/javascript" src="{{ asset('assets/js/bio-forms.js') }}?ver=1.0.0"></script>
 	@yield('js')
-	<script type="text/javascript" src="js/carrito.js?ver=2.0.2"></script>
+	<script type="text/javascript" src="js/carrito.js?ver=2.0.3"></script>
 	
 	<script>
 
