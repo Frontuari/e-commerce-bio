b0aa06bcae4dc0460e4884517907368e0a563b07
Carlos Vargas <cvargas@frontuari.net>
2020-12-02 09:44:39 -0400

    add the images for coorporative zone

--- resources/js/components/Ads.vue
+++ resources/js/components/Ads.vue
@@ -2,16 +2,16 @@
     <section id="categories">
 		<div class="row">
 			<div class="col-lg-3 col-md-6 col-sm-12 col-xs-12">
-				<a href="#"><div class="category-img" style="background-image: url('assets/img/sucursales/el_bosque.jpg'); background-repeat: no-repeat;"></div></a>
+				<a href="/contact"><div class="category-img" style="background-image: url('assets/img/sucursales/contacto.jpg'); background-repeat: no-repeat;"></div></a>
 			</div>
 			<div class="col-lg-3 col-md-6 col-sm-12 col-xs-12">
-				<a href="#"><div class="category-img" style="background-image: url('assets/img/sucursales/la_granja.jpg'); background-repeat: no-repeat;"></div></a>
+				<a href="/culture"><div class="category-img" style="background-image: url('assets/img/sucursales/cultura.jpg'); background-repeat: no-repeat;"></div></a>
 			</div>
 			<div class="col-lg-3 col-md-6 col-sm-12 col-xs-12">
-				<a href="#"><div class="category-img" style="background-image: url('assets/img/sucursales/paraparal.jpg'); background-repeat: no-repeat;"></div></a>
+				<a href="#"><div class="category-img" style="background-image: url('assets/img/sucursales/proveedores.jpg'); background-repeat: no-repeat;"></div></a>
 			</div>
 			<div class="col-lg-3 col-md-6 col-sm-12 col-xs-12">
-				<a href="#"><div class="category-img" style="background-image: url('assets/img/sucursales/santa_cecilia.jpg'); background-repeat: no-repeat;"></div></a>
+				<a href="/sucursal"><div class="category-img" style="background-image: url('assets/img/sucursales/sucursales.jpg'); background-repeat: no-repeat;"></div></a>
 			</div>
 		</div>
 	</section>
