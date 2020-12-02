8f730936133a7c05c042d95d72940fb0238c76e6
Carlos Vargas <cvargas@frontuari.net>
2020-12-02 09:51:31 -0400

    add the proveedores links

--- resources/js/components/Ads.vue
+++ resources/js/components/Ads.vue
@@ -8,7 +8,7 @@
 				<a href="/culture"><div class="category-img" style="background-image: url('assets/img/sucursales/cultura.jpg'); background-repeat: no-repeat;"></div></a>
 			</div>
 			<div class="col-lg-3 col-md-6 col-sm-12 col-xs-12">
-				<a href="#"><div class="category-img" style="background-image: url('assets/img/sucursales/proveedores.jpg'); background-repeat: no-repeat;"></div></a>
+				<a href="http://portalproveedores.biomercados.com.ve:18880/webui/"><div class="category-img" style="background-image: url('assets/img/sucursales/proveedores.jpg'); background-repeat: no-repeat;"></div></a>
 			</div>
 			<div class="col-lg-3 col-md-6 col-sm-12 col-xs-12">
 				<a href="/sucursal"><div class="category-img" style="background-image: url('assets/img/sucursales/sucursales.jpg'); background-repeat: no-repeat;"></div></a>
