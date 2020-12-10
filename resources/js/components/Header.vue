3905c5de0899bea280b80723dd9c01ece2c18eba
Carlos Vargas <cvargas@frontuari.net>
2020-12-10 10:24:35 -0400

    ajust the category image to center

--- resources/js/components/Header.vue
+++ resources/js/components/Header.vue
@@ -92,11 +92,11 @@
 		</div>
 		<div class="container-fluid">
 			<div class="row">
-				<div class="col-lg-2 col-md-4 col-sm-6 col-xs-6" v-for="cat in categories.slice(0,12)" :key="cat.id" :style="'background:url(/storage/'+cat.image+'); background-repeat: no-repeat; background-size: auto;'">
+				<div class="col-lg-2 col-md-4 col-sm-6 col-xs-6" v-for="cat in categories.slice(0,12)" :key="cat.id" :style="'background:url(/storage/'+cat.image+'); background-repeat: no-repeat; background-size: auto; background-position: center;'">
 					<a class="new-cat" :href="'/catalog?cat='+cat.id" style="margin-top: 5px"><strong style="visibility: hidden;" :id="'cat_'+cat.id">{{ cat.name }}</strong></a>
 				</div>
 
-				<div class="col-lg-2 col-md-4 col-sm-6 col-xs-6" style="background:url('assets/img/bio-insuperable.svg'); background-repeat: no-repeat; background-size: auto;">
+				<div class="col-lg-2 col-md-4 col-sm-6 col-xs-6" style="background:url('assets/img/bio-insuperable.svg'); background-repeat: no-repeat; background-size: auto; background-position: center;">
 					<a class="new-cat" href="catalog?tag=BIOINSUPERABLE" style="margin-top: 5px"><strong style="visibility: hidden;" id="">BIOINSUPERABLE</strong></a>
 				</div>
 			</div>
