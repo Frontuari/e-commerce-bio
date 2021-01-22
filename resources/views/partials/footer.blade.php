8f36c9e0a6ed903515bce43fadc0a5377fe854f4
Carlos Vargas <cvargas@frontuari.net>
2021-01-22 02:40:01 -0400

    change version

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -102,10 +102,10 @@
 	<div class="over toggle-menu"></div>
 
 </main>
-	<script type="text/javascript" src="{{ mix('js/app.js') }}&ver=3.0.11"></script>
+	<script type="text/javascript" src="{{ mix('js/app.js') }}&ver=3.0.12"></script>
 	@yield('js')
-	<script type="text/javascript" src="{{ asset('assets/js/bio-forms.js') }}?ver=3.0.11"></script>
-	<script type="text/javascript" src="js/carrito.min.js?ver=3.0.11"></script>
+	<script type="text/javascript" src="{{ asset('assets/js/bio-forms.js') }}?ver=3.0.12"></script>
+	<script type="text/javascript" src="js/carrito.min.js?ver=3.0.12"></script>
 	<script>
 
 		$("span#eye-view-password").click(function(){
