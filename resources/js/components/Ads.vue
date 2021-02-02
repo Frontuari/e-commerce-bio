3c7c84c3cf434cd459003b09d47e15d6b2729b2c
Carlos Vargas <cvargas@frontuari.net>
2021-02-02 16:46:16 -0400

    ajust the coorporative images backgrounds

--- resources/js/components/Ads.vue
+++ resources/js/components/Ads.vue
@@ -1,17 +1,17 @@
 <template>
     <section id="categories">
 		<div class="row">
-			<div class="col-lg-3 col-md-6 col-sm-12 col-xs-12">
-				<a href="/contact"><div class="category-img" style="background-image: url('assets/img/sucursales/contacto.svg'); background-repeat: no-repeat;"></div></a>
+			<div class="col-lg-3 col-md-6 col-sm-6 col-xs-12">
+				<a href="/contact"><div class="category-img" style="background-image: url('assets/img/sucursales/contacto.svg'); background-repeat: no-repeat; background-size: 100% 100%;"></div></a>
 			</div>
-			<div class="col-lg-3 col-md-6 col-sm-12 col-xs-12">
-				<a href="/culture"><div class="category-img" style="background-image: url('assets/img/sucursales/cultura.svg'); background-repeat: no-repeat;"></div></a>
+			<div class="col-lg-3 col-md-6 col-sm-6 col-xs-12">
+				<a href="/culture"><div class="category-img" style="background-image: url('assets/img/sucursales/cultura.svg'); background-repeat: no-repeat; background-size: 100% 100%;"></div></a>
 			</div>
-			<div class="col-lg-3 col-md-6 col-sm-12 col-xs-12">
-				<a href="http://portalproveedores.biomercados.com.ve:18880/webui/"><div class="category-img" style="background-image: url('assets/img/sucursales/portal.svg'); background-repeat: no-repeat;"></div></a>
+			<div class="col-lg-3 col-md-6 col-sm-6 col-xs-12">
+				<a href="http://portalproveedores.biomercados.com.ve:18880/webui/"><div class="category-img" style="background-image: url('assets/img/sucursales/portal.svg'); background-repeat: no-repeat; background-size: 100% 100%;"></div></a>
 			</div>
-			<div class="col-lg-3 col-md-6 col-sm-12 col-xs-12">
-				<a href="/sucursal"><div class="category-img" style="background-image: url('assets/img/sucursales/sucursales.svg'); background-repeat: no-repeat;"></div></a>
+			<div class="col-lg-3 col-md-6 col-sm-6 col-xs-12">
+				<a href="/sucursal"><div class="category-img" style="background-image: url('assets/img/sucursales/sucursales.svg'); background-repeat: no-repeat; background-size: 100% 100%;"></div></a>
 			</div>
 		</div>
 	</section>
